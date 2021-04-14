1201f01bbdbf2d2cd0676ad7f26219f3b39d98f1
rpantalena <rjpantalena@gmail.com>
2021-04-14 15:15:51 -0400

    maid service

--- src/components/common/button/Button.js
+++ src/components/common/button/Button.js
@@ -12,20 +12,5 @@ export default function Button({
   buttonStyle,
   buttonSize,
 }) {
-  const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
-
-  const checkButtonSize = SIZES.includes(buttonSize) 
-    ? buttonSize 
-    : SIZES[0];
-
-  return (
-    <Link to='/sign-up' className='btn-mobile'>
-      <button
-      className={'btn ${checkButtonStyle} ${checkButtonTye}'}>
-        {children}
-      </button>
-    </Link>
-  );
-};
+  return <Link></Link>;
+}
